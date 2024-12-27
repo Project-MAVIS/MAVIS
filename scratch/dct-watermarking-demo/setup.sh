7fd3f97ab021b27adc5808a71aef07c59df9388e
Shantanu <shantanuwable2003@gmail.com>
2024-12-27 13:21:22 +0530

    Add pixeif to setup script dependencies

--- scratch/dct-watermarking-demo/setup.sh
+++ scratch/dct-watermarking-demo/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install streamlit cryptography opencv-python scipy pywavelets pillow
+pip install streamlit cryptography opencv-python scipy pywavelets pillow pixeif
 
 # Generate keys
 sh keys.sh
